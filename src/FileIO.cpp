25a503e117a3f3dc0f1842a5f4541b5aa4244349
Maxim Timoshkin <maximatimoshkin@outlook.com>
2024-02-04 01:56:51 +0300

    SplitBufferToLines() fix

--- src/FileIO.cpp
+++ src/FileIO.cpp
@@ -117,8 +117,6 @@ size_t SplitBufferToLines (char *file_buffer, TextBuffer *text_buffer, char deli
         current_symbol++;
     }
 
-    current_line_index++;
-
     if (text_buffer != NULL) {
         text_buffer->lines[current_line_index - 1].length = LineLength (previous_symbol, delim);
         text_buffer->line_count = current_line_index;
